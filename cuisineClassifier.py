6862ed29b93457df18e23dce2749741252fa5e8a
Monty Carter <montgomery.carter@gmail.com>
2015-12-14 19:35:23 -0700

    Adds basic svm training using 500 examples, and 5 test data

--- cuisineClassifier.py
+++ cuisineClassifier.py
@@ -1,10 +1,19 @@
 import sklearn
 from vectorizeRecipes import toSklearnFormat
+from sklearn import svm
 from pprint import pprint
 
 trainFile = 'srcData/train.json'
 testFile = 'srcData/test.json'
 
+#Get dataset object in sklearn format
 recData = toSklearnFormat(trainFile=trainFile, testFile=testFile)
-
-pprint(recData)
+#Instantiate the svm classifier
+clf = svm.SVC(gamma=0.001, C=100.)
+#Train the svm classifier (saving 5 as test data)
+clf.fit(recData['data'][:500], recData['target'][:500])
+#Predict the last 5 labels
+pred = clf.predict(recData['data'][-5:])
+#Print the last 5 predicted labels, and the last 5 real labels
+print(pred)
+print(recData['target'][-5:])
